86e9c7761ccfe6b7729a091324146cd709bd9072
oscargabriel <ozambranopinto@gmail.com>
2023-02-18 14:19:59 -0400

    Initial commit

---
+++ src/main/java/com/example/HibernatePostgreSecurityJWT/HibernatePostgreSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.HibernatePostgreSecurityJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatePostgreSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatePostgreSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HibernatePostgreSecurityJWT/HibernatePostgreSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HibernatePostgreSecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatePostgreSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
