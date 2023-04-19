113d0b67670c8a4c7e4ce067ef9aecbb8ba8ce31
oscargabriel <ozambranopinto@gmail.com>
2023-04-19 08:11:19 -0500

    test unitario para existByEmail cuando no encuentra el email dado

---
+++ src/test/java/com/example/HibernatePostgreSecurityJWT/repsitory/dao/Impl/RepositorySqlImplTest.java
@@ -0,0 +1,58 @@
+package com.example.HibernatePostgreSecurityJWT.repsitory.dao.Impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RepositorySqlImplTest {
+
+    @Autowired
+    RepositorySqlImpl repositorySql;
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void notExistByEmail() {
+        String email = "adasdasdasd";
+        Boolean result = repositorySql.existByEmail(email);
+        assertEquals(false, result);
+    }
+
+    @Test
+    void existByUsername() {
+    }
+
+    @Test
+    void existByDocument() {
+    }
+
+    @Test
+    void findAllUser() {
+    }
+
+    @Test
+    void findUserByUsername() {
+    }
+
+    @Test
+    void findRoleByNameRol() {
+    }
+
+    @Test
+    void findRolesByUsername() {
+    }
+
+    @Test
+    void findIdUserRoleByUserId() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+}
